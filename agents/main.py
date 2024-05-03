b6598c2fc37eba5ce49a2a1a9812bf05db78dfdb
Durmus Demirtas <demirtasdurmus@gmail.com>
2024-05-03 17:52:14 +0300

    section6: start agents and agent executors

---
+++ agents/main.py
@@ -0,0 +1,38 @@
+# Load env variables
+from dotenv import load_dotenv;
+load_dotenv(dotenv_path='../.env')
+
+from langchain_openai import ChatOpenAI
+from langchain.prompts import (
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder
+)
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+
+from tools.sql import run_query_tool
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+  messages=[
+    HumanMessagePromptTemplate.from_template('{input}'),
+    MessagesPlaceholder(variable_name='agent_scratchpad')
+  ]
+)
+
+tools = [run_query_tool]
+
+agent = OpenAIFunctionsAgent(
+  llm=chat,
+  prompt=prompt,
+  tools=tools
+)
+
+agent_executor = AgentExecutor(
+  agent=agent,
+  verbose=True,
+  tools=tools
+)
+
+agent_executor('How many users are in the database?')
